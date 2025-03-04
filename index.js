5476a7359c54d87bafb7d522b9671afd8612dc77
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-03-04 12:32:45 +0300

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-
 // Write your code here
+
+// function multiply(num1, num2) {
+//   var result = num1 * num2;
+//   return 62;
+// }
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * 100) + 1; 
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4; 
+
+let max = Math.max(1, 5, 10, 15, 20); 
+
+console.log(random); 
+console.log(mod);    
+console.log(max);    
+
